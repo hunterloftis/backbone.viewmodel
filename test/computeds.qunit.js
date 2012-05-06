e073f7fadc7c09a97d993ca3f41583c7c0e7f2da
Hunter Loftis <hunter@hunterloftis.com>
2012-05-06 02:17:14 -0400

    adds test for nested computations

--- test/computeds.qunit.js
+++ test/computeds.qunit.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   module("Backbone.ViewModel.compute()");
 
   test("tracking a simple computation within a ViewModel", function() {
-
     var computations = 0;
 
     var vm = new Backbone.ViewModel({
@@ -21,10 +20,11 @@ $(document).ready(function() {
     vm.set('base', 2);
     strictEqual(vm.get('triple'), 6, 'computation should be 6 after base update');
     strictEqual(computations, 2, 'computation should be run twice');
+
+    strictEqual(Backbone.Computed._computations.length, 0, 'computation stack should be clear');
   });
 
   test("tracking a simple computation across ViewModels", function() {
-
     var computations = 0;
 
     var a = new Backbone.Model({
@@ -46,11 +46,11 @@ $(document).ready(function() {
     a.set('base', 2);
     strictEqual(b.get('triple'), 6, 'computation should be 6 after base update');
     strictEqual(computations, 2, 'computation should be run twice');
+
+    strictEqual(Backbone.Computed._computations.length, 0, 'computation stack should be clear');
   });
 
-
   test("filtering a model", function() {
-
     var computations = 0;
 
     var model = new Backbone.Model({
@@ -76,8 +76,7 @@ $(document).ready(function() {
         return b.get('points') - a.get('points');
       };
       players.sort();
-      var result = players.first(limit);
-      return result;
+      return players.first(limit);
     });
 
     var tops = vm.get('topPlayers');
@@ -92,6 +91,42 @@ $(document).ready(function() {
     strictEqual(tops[0].get('name'), 'Edgar', 'Top player should be Edgar after add');
     strictEqual(computations, 2, 'Two computations should have been run');
 
+    strictEqual(Backbone.Computed._computations.length, 0, 'computation stack should be clear');
+  });
+
+  test("tracking nested computations", function() {
+    var computations = 0;
+
+    var a = new Backbone.ViewModel({
+      first: 'Hunter',
+      last: 'Loftis'
+    });
+
+    var b = new Backbone.ViewModel();
+    b.compute('fullname', function() {
+      return a.get('first') + ' ' + a.get('last');
+    });
+
+    var c = new Backbone.ViewModel({
+      label: 'Full name: '
+    });
+    c.compute('caption', function() {
+      computations++;
+      return this.get('label') + b.get('fullname');
+    });
+
+    strictEqual(c.get('caption'), 'Full name: Hunter Loftis', 'caption should be Full name: Hunter Loftis');
+    strictEqual(computations, 1, 'one compuation should have run');
+
+    a.set('first', 'Brooke');
+    strictEqual(c.get('caption'), 'Full name: Brooke Loftis', 'caption should be Full name: Brooke Loftis');
+    strictEqual(computations, 2, 'two computations should have run');
+
+    c.set('label', 'My name: ');
+    strictEqual(c.get('caption'), 'My name: Brooke Loftis', 'caption should be My name: Brooke Loftis');
+    strictEqual(computations, 3, 'three computations should have run');
+
+    strictEqual(Backbone.Computed._computations.length, 0, 'computation stack should be clear');
   });
 
 });
