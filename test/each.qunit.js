06e722fc1b59413846f465dac0e6e9e8de95eea5
Hunter Loftis <hunter@hunterloftis.com>
2012-05-12 09:35:44 -0400

    started fragment tests

--- test/each.qunit.js
+++ test/each.qunit.js
@@ -2,15 +2,24 @@ $(document).ready(function() {
 
   module("Backbone.Binding.each");
 
-  test("basic rendering", function() {
+  test("rendering and updating", function() {
+
+    var list1 = new Backbone.Collection([
+      { label: 'a' },
+      { label: 'b' },
+      { label: 'c' },
+      { label: 'd' }
+    ]);
+
+    var list2 = new Backbone.Collection([
+      { label: 'e' },
+      { label: 'f' },
+      { label: 'g' },
+      { label: 'h' }
+    ]);
 
     var vm = new Backbone.Model({
-      listItems: new Backbone.Collection([
-        { label: 'a' },
-        { label: 'b' },
-        { label: 'c' },
-        { label: 'd' }
-      ])
+      listItems: list1
     });
 
     var children = $('#simpleEach').children();
@@ -23,9 +32,39 @@ $(document).ready(function() {
     children = $('#simpleEach').children();
     strictEqual(children.length, 4, 'UL should have four children');
     strictEqual($(children[0]).text(), 'a', 'first LI should be "a"');
-    strictEqual($(children[1]).text(), 'b', 'first LI should be "b"');
-    strictEqual($(children[2]).text(), 'c', 'first LI should be "c"');
-    strictEqual($(children[3]).text(), 'd', 'first LI should be "d"');
+    strictEqual($(children[1]).text(), 'b', 'second LI should be "b"');
+    strictEqual($(children[2]).text(), 'c', 'third LI should be "c"');
+    strictEqual($(children[3]).text(), 'd', 'fourth LI should be "d"');
+
+    vm.get('listItems').pop();
+    children = $('#simpleEach').children();
+    strictEqual(children.length, 3, 'UL should have three children after pop');
+    strictEqual($(children[2]).text(), 'c', 'third LI should be "c"');
+
+    vm.set('listItems', list2);
+    children = $('#simpleEach').children();
+    strictEqual(children.length, 4, 'UL should have four children');
+    strictEqual($(children[0]).text(), 'e', 'first LI should be "e"');
+    strictEqual($(children[1]).text(), 'f', 'second LI should be "f"');
+    strictEqual($(children[2]).text(), 'g', 'third LI should be "g"');
+    strictEqual($(children[3]).text(), 'h', 'fourth LI should be "h"');
+
+    vm.get('listItems').shift();
+    children = $('#simpleEach').children();
+    strictEqual(children.length, 3, 'UL should have three children after pop');
+    strictEqual($(children[0]).text(), 'f', 'first LI should be "f"');
+  });
+
+  test("rendering fragments", function() {
+    expect(0);
+  });
+
+  test("rendering the DOM efficiently", function() {
+    expect(0);
+    var vm = new Backbone.ViewModel({
+      items: new Backbone.Collection()
+    });
+
   });
 
 });
